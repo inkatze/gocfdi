8a0b245ef208a879b2b0da6b6cf30a6f4098cdb8
Diego Romero <jd@inkatze.com>
2019-11-29 15:43:08 -0800

    WIP adds digested result

--- cfdi.go
+++ cfdi.go
@@ -10,23 +10,33 @@ import (
 // ValidationURL for the SOAP service to validate CFDI documents
 const ValidationURL = "https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc?wsdl"
 
+const successMessage = "S - Comprobante obtenido satisfactoriamente."
+const invalidInvoice = "N - 601: La expresión impresa proporcionada no es válida."
+const invalidNotFound = "N - 602: Comprobante no encontrado"
+
 // InvoiceHeaders values used to uniquely identify CFDI documents
 type InvoiceHeaders struct {
 	IssuerRFC, AddresseeRFC, TotalAmount, InvoiceUUID string
 	Client                                            *gosoap.Client
 }
 
-// ConsultaResponse has the request's unmarshalled values
-type ConsultaResponse struct {
+// ValidationResult contains the processed results from the validation response
+type ValidationResult struct {
+	RawResponse string
+}
+
+type consultaResponse struct {
 	XMLName            xml.Name `xml:"ConsultaResponse"`
 	ResponseStatus     string   `xml:"ConsultaResult>CodigoEstatus"`
 	CFDIStatus         string   `xml:"ConsultaResult>Estado"`
 	CancellationStatus string   `xml:"ConsultaResult>EstatusCancelacion"`
 	IsCancellable      string   `xml:"ConsultaResult>EsCancelable"`
-	RawResponse        string
 }
 
-var r ConsultaResponse
+var (
+	c consultaResponse
+	r ValidationResult
+)
 
 // SoapClient creates a new SOAP client for the given url.
 func (i *InvoiceHeaders) SoapClient(url string) error {
@@ -40,7 +50,7 @@ func (i *InvoiceHeaders) SoapClient(url string) error {
 
 // ValidateDocument checks if the document with the given parameters exists in SAT's system and is valid.
 // BUG(inkatze): The namespace is not being parsed correctly in gosoap.
-func ValidateDocument(i InvoiceHeaders) (ConsultaResponse, error) {
+func ValidateDocument(i InvoiceHeaders) (ValidationResult, error) {
 	if i.Client == nil {
 		if err := i.SoapClient(ValidationURL); err != nil {
 			return r, err
@@ -65,11 +75,22 @@ func ValidateDocument(i InvoiceHeaders) (ConsultaResponse, error) {
 	if err != nil {
 		return r, fmt.Errorf(callError, err)
 	}
-	r.RawResponse = string(res.Body)
-	err = res.Unmarshal(&r)
+	err = res.Unmarshal(&c)
 	if err != nil {
 		return r, fmt.Errorf("Error while unmarshalling response: %w", err)
 	}
 
+	switch c.ResponseStatus {
+	case invalidInvoice:
+		return r, fmt.Errorf("The given parameters for the CFDI document are invalid: %s", c.ResponseStatus)
+	case invalidNotFound:
+		return r, fmt.Errorf("Couldn't find a CFDI document with the given parameters")
+	default:
+		if c.ResponseStatus != successMessage {
+			return r, fmt.Errorf("Unrecognized status response %s", c.ResponseStatus)
+
+		}
+	}
+	r.RawResponse = string(res.Body)
 	return r, nil
 }
