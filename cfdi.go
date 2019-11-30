1c3c3c522d141d915f76578ef6c36f3c39b9db45
Diego Romero <jd@inkatze.com>
2019-11-29 16:13:57 -0800

    Adds validation functions to struct

--- cfdi.go
+++ cfdi.go
@@ -15,8 +15,8 @@ const successMessage = "S - Comprobante obtenido satisfactoriamente."
 const invalidInvoice = "N - 601: La expresión impresa proporcionada no es válida."
 const invalidNotFound = "N - 602: Comprobante no encontrado"
 
-// InvoiceHeaders values used to uniquely identify CFDI documents
-type InvoiceHeaders struct {
+// DocumentHeaders values used to uniquely identify CFDI documents
+type DocumentHeaders struct {
 	IssuerRFC, AddresseeRFC, TotalAmount, InvoiceUUID string
 	Client                                            *gosoap.Client
 }
@@ -40,43 +40,51 @@ var (
 )
 
 // SoapClient creates a new SOAP client for the given url.
-func (i *InvoiceHeaders) SoapClient(url string) error {
+func (d *DocumentHeaders) SoapClient(url string) error {
 	client, err := gosoap.SoapClient(url)
 	if err != nil {
 		log.Errorf("Error while creating SOAP client for url: %s", url)
 		return fmt.Errorf("Error while creating a SOAP client for %s: %w", url, err)
 	}
 	log.Debugf("SOAP client successfully created")
-	i.Client = client
+	d.Client = client
 	return nil
 }
 
-// ValidateDocument checks if the document with the given parameters exists in SAT's system and is valid.
-// BUG(inkatze): The namespace is not being parsed correctly in gosoap.
-func ValidateDocument(i InvoiceHeaders) (ValidationResult, error) {
-	if i.Client == nil {
-		if err := i.SoapClient(ValidationURL); err != nil {
-			return r, err
+func (d *DocumentHeaders) call() (*gosoap.Response, error) {
+	if d.Client == nil {
+		if err := d.SoapClient(ValidationURL); err != nil {
+			return nil, err
 		}
 	}
 	parsedValues := fmt.Sprintf(
 		"re=%s&rr=%s&tt=%s&id=%s",
-		i.IssuerRFC, i.AddresseeRFC, i.TotalAmount, i.InvoiceUUID,
+		d.IssuerRFC, d.AddresseeRFC, d.TotalAmount, d.InvoiceUUID,
 	)
 	params := gosoap.Params{"expresionImpresa": parsedValues}
 
 	// Work around a bug that uses the wrong namespace
-	res, err := i.Client.Call("Consulta", params)
+	res, err := d.Client.Call("Consulta", params)
 	callError := "Error while calling SOAP action: %w"
 	if err != nil {
-		return r, fmt.Errorf(callError, err)
+		return res, fmt.Errorf(callError, err)
 	}
-	i.Client.Definitions.Types[0].XsdSchema[0].TargetNamespace = "http://tempuri.org/"
+	d.Client.Definitions.Types[0].XsdSchema[0].TargetNamespace = "http://tempuri.org/"
 
 	// Actual call
-	res, err = i.Client.Call("Consulta", params)
+	res, err = d.Client.Call("Consulta", params)
 	if err != nil {
-		return r, fmt.Errorf(callError, err)
+		return res, fmt.Errorf(callError, err)
+	}
+	return res, nil
+}
+
+// Validate checks if the document with the given parameters exists in SAT's system and is valid.
+// BUG(inkatze): The namespace is not being parsed correctly in gosoap.
+func (d *DocumentHeaders) Validate() (ValidationResult, error) {
+	res, err := d.call()
+	if err != nil {
+		// Error
 	}
 	err = res.Unmarshal(&c)
 	if err != nil {
--- cfdi_test.go
+++ cfdi_test.go
@@ -5,13 +5,13 @@ import (
 )
 
 func TestValidation(t *testing.T) {
-	invoice := InvoiceHeaders{
+	document := DocumentHeaders{
 		IssuerRFC:    "LSO1306189R5",
 		AddresseeRFC: "GACJ940911ASA",
 		TotalAmount:  "4999.99",
 		InvoiceUUID:  "e7df3047-f8de-425d-b469-37abe5b4dabb",
 	}
-	response, err := ValidateDocument(invoice)
+	response, err := document.Validate()
 	if err != nil {
 		t.Errorf("Failed to validate CFDI document: %w", err)
 	}
