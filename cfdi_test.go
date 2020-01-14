548f9ff8e34e0b63c048164bf3d244bca258a8b3
Diego Romero <jd@inkatze.com>
2020-01-14 13:36:27 -0600

    Adds logging and slightly better variable names
    **CHANGELOG:**
    
    - Adds more logging messages.
    - Changed a few variables and fields names.
    - Fixed an incorrect function name in the README.

--- cfdi_test.go
+++ cfdi_test.go
@@ -5,15 +5,20 @@ import (
 )
 
 func TestValidation(t *testing.T) {
-	document := DocumentHeader{
-		IssuerRFC:    "LSO1306189R5",
-		AddresseeRFC: "GACJ940911ASA",
-		TotalAmount:  "4999.99",
-		InvoiceUUID:  "e7df3047-f8de-425d-b469-37abe5b4dabb",
+	issuer := "LSO1306189R5"
+	addressee := "GACJ940911ASA"
+	total := "4999.99"
+	uuid := "e7df3047-f8de-425d-b469-37abe5b4dabb"
+
+	invoice := InvoiceHeader{
+		IssuerRFC:    issuer,
+		AddresseeRFC: addressee,
+		TotalAmount:  total,
+		UUID:         uuid,
 	}
-	response, err := document.Validate()
+	result, err := invoice.Validate()
 	if err != nil {
 		t.Errorf("Failed to validate CFDI document: %w", err)
 	}
-	t.Logf("Got valid response object %v", response)
+	t.Logf("Got valid response object %+v", *result)
 }
